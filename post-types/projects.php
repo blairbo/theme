226b6d08148ba9f5041a107154e632a7f123ba43
Jack <jack@jacks.site>
2019-04-09 14:18:02 +0200

    added post type helper text

--- post-types/projects.php
+++ post-types/projects.php
@@ -1,53 +1,57 @@
 <?php
+/**
+ * Example Custom post-type. 
+ * Creating seprate files for each post-type in the [post-type] folder
+ * will register them automaitcally.
+ *
+ * @package  HeadlessPress
+ */
 
-function hp_pt_register_projects() {
 
-	/**
-	 * Post Type: Projects.
-	 */
+ // add_action( 'init', 'hp_pt_register_projects' );
 
-	$labels = array(
-		"name" => __( "Projects", "headlesspress" ),
-		"singular_name" => __( "Project", "headlesspress" ),
-		"menu_name" => __( "Projects", "headlesspress" ),
-		"all_items" => __( "All Projects", "headlesspress" ),
-		"add_new" => __( "Add Project", "headlesspress" ),
-		"add_new_item" => __( "Add new Project", "headlesspress" ),
-		"edit_item" => __( "Edit Project", "headlesspress" ),
-		"new_item" => __( "New Project", "headlesspress" ),
-		"view_item" => __( "View Project", "headlesspress" ),
-		"view_items" => __( "View Projects", "headlesspress" ),
-		"search_items" => __( "Search Projects", "headlesspress" ),
-		"not_found" => __( "No Projects Found", "headlesspress" ),
-		"not_found_in_trash" => __( "No Projects Found In Trash", "headlesspress" ),
-	);
+// function hp_pt_register_projects() {
 
-	$args = array(
-		"label" => __( "Projects", "headlesspress" ),
-		"labels" => $labels,
-		"description" => "",
-		"public" => true,
-		"publicly_queryable" => true,
-		"show_ui" => true,
-		"delete_with_user" => false,
-		"show_in_rest" => true,
-		"rest_base" => "projects",
-		"has_archive" => false,
-		"show_in_menu" => true,
-		"show_in_nav_menus" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "page",
-		"map_meta_cap" => true,
-		"hierarchical" => true,
-		"rewrite" => array( "slug" => "project", "with_front" => true ),
-		"query_var" => true,
-		"supports" => array( "title", "editor", "thumbnail", "author" ),
-		"taxonomies" => array('post_tag', 'category' ),
-	);
+// 	$labels = array(
+// 		"name" => __( "Projects", "headlesspress" ),
+// 		"singular_name" => __( "Project", "headlesspress" ),
+// 		"menu_name" => __( "Projects", "headlesspress" ),
+// 		"all_items" => __( "All Projects", "headlesspress" ),
+// 		"add_new" => __( "Add Project", "headlesspress" ),
+// 		"add_new_item" => __( "Add new Project", "headlesspress" ),
+// 		"edit_item" => __( "Edit Project", "headlesspress" ),
+// 		"new_item" => __( "New Project", "headlesspress" ),
+// 		"view_item" => __( "View Project", "headlesspress" ),
+// 		"view_items" => __( "View Projects", "headlesspress" ),
+// 		"search_items" => __( "Search Projects", "headlesspress" ),
+// 		"not_found" => __( "No Projects Found", "headlesspress" ),
+// 		"not_found_in_trash" => __( "No Projects Found In Trash", "headlesspress" ),
+// 	);
 
-	register_post_type( "project", $args );
-}
+// 	$args = array(
+// 		"label" => __( "Projects", "headlesspress" ),
+// 		"labels" => $labels,
+// 		"description" => "",
+// 		"public" => true,
+// 		"publicly_queryable" => true,
+// 		"show_ui" => true,
+// 		"delete_with_user" => false,
+// 		"show_in_rest" => true,
+// 		"rest_base" => "projects",
+// 		"has_archive" => false,
+// 		"show_in_menu" => true,
+// 		"show_in_nav_menus" => true,
+// 		"exclude_from_search" => false,
+// 		"capability_type" => "page",
+// 		"map_meta_cap" => true,
+// 		"hierarchical" => true,
+// 		"rewrite" => array( "slug" => "project", "with_front" => true ),
+// 		"query_var" => true,
+// 		"supports" => array( "title", "editor", "thumbnail", "author" ),
+// 		"taxonomies" => array('post_tag', 'category' ),
+// 	);
 
-add_action( 'init', 'hp_pt_register_projects' );
+// 	register_post_type( "project", $args );
+// }
 
 
